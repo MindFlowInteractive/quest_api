aedd3346b226090e088676296fa534557c1ebee9
Yusuf Falade <faladeyusuf54@gmail.com>
2025-08-24 12:11:12 +0100

    index

---
+++ src/puzzle-analytics/index.ts
@@ -0,0 +1,7 @@
+// Barrel exports for easy importing
+export { PuzzleAnalyticsModule } from "./puzzle-analytics.module"
+export { PuzzleAnalyticsService } from "./services/puzzle-analytics.service"
+export { PuzzleAnalytics } from "./entities/puzzle-analytics.entity"
+export { CreatePuzzleAnalyticsDto } from "./dto/create-puzzle-analytics.dto"
+export { PuzzleStatsDto, UserStatsDto, AnalyticsOverviewDto } from "./dto/puzzle-stats.dto"
+export { PuzzleAnalyticsController } from "./controllers/puzzle-analytics.controller"
